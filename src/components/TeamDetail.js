88aef0de3d07de27a24cbcedf879ba8791844a62
hassantauqeer <tauqeerhassan66@gmail.com>
2018-01-25 17:14:35 +0500

    first commit

---
+++ src/components/TeamDetail.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TeamDetail extends React.Component {
+    render() {
+        return ()
+    }
+}
+
+$NAME.propTypes = {
+    //myProps = React.PropTypes.string.isRequired
+}
+
+export default TeamDetail;
